1742407bf37cc3c7deec98a490d13dbb9f219822
Han Yolo <simon.herzog@protonmail.ch>
2017-09-29 15:55:53 +0200

    std_psh

--- TEAM_Remote/Sources/main.c
+++ TEAM_Remote/Sources/main.c
@@ -28,6 +28,8 @@
 
 
 /* Including needed modules to compile this module/procedure */
+
+/* Including shared modules, which are used for whole project */
 #include "Cpu.h"
 #include "Events.h"
 #include "MCUC1.h"
@@ -36,12 +38,6 @@
 #include "TI1.h"
 #include "TimerIntLdd1.h"
 #include "TU1.h"
-#include "WAIT1.h"
-#include "UTIL1.h"
-#include "KIN1.h"
-#include "XF1.h"
-#include "IFsh1.h"
-#include "IntFlashLdd1.h"
 #include "CLS1.h"
 #include "AS1.h"
 #include "ASerialLdd1.h"
@@ -68,11 +64,16 @@
 #include "CDC1.h"
 #include "Tx1.h"
 #include "Rx1.h"
-#include "TmDt1.h"
 #include "TMOUT1.h"
+#include "WAIT1.h"
+#include "UTIL1.h"
 #include "HF1.h"
 #include "CS1.h"
-/* Including shared modules, which are used for whole project */
+#include "KIN1.h"
+#include "XF1.h"
+#include "IFsh1.h"
+#include "IntFlashLdd1.h"
+#include "TmDt1.h"
 #include "PE_Types.h"
 #include "PE_Error.h"
 #include "PE_Const.h"
