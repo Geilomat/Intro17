765cf297ba50f62eda71d87ed7df6c3a653615c4
Geilomat <michikurmann@hotmail.com>
2017-10-06 16:26:58 +0200

    The Holy Grail of C

--- TEAM_Common/Application.c
+++ TEAM_Common/Application.c
@@ -185,12 +185,14 @@ void APP_Start(void) {
   __asm volatile("cpsie i"); /* enable interrupts */
 
 
-  MOT_SetSpeedPercent(MOT_GetMotorHandle(MOT_MOTOR_LEFT),50);
-  MOT_SetSpeedPercent(MOT_GetMotorHandle(MOT_MOTOR_RIGHT),50);
+  //MOT_SetSpeedPercent(MOT_GetMotorHandle(MOT_MOTOR_LEFT),50);
+  //MOT_SetSpeedPercent(MOT_GetMotorHandle(MOT_MOTOR_RIGHT),50);
 
   for(;;) {
+	  EnterCritical();
 	  LED2_On();
 	  LED1_Off();
+	  ExitCritical();
 	  WAIT1_Waitms(200);
 	  LED1_On();
   	  LED2_Off();
